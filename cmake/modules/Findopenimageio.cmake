7bb428df478e1c1eb66e16a1f78cf541f7dab056
Silverlan <silverlann@gmail.com>
2025-07-01 14:42:25 +0200

    build: update cmake script

--- cmake/modules/Findopenimageio.cmake
+++ cmake/modules/Findopenimageio.cmake
@@ -22,12 +22,32 @@ find_library(${PCK}_UTIL_LIBRARY
     ${PRAGMA_DEPS_DIR}/openimageio/lib
 )
 
+set(REQ_VARS ${PCK}_LIBRARY ${PCK}_UTIL_LIBRARY ${PCK}_INCLUDE_DIR)
+if(WIN32)
+  find_file(
+    ${PCK}_OIIO_RUNTIME
+    NAMES OpenImageIO.dll
+    HINTS
+      ${PRAGMA_DEPS_DIR}/openimageio/bin
+  )
+  find_file(
+    ${PCK}_OIIO_UTIL_RUNTIME
+    NAMES OpenImageIO_Util.dll
+    HINTS
+      ${PRAGMA_DEPS_DIR}/openimageio/bin
+  )
+  set(REQ_VARS ${REQ_VARS} ${PCK}_OIIO_RUNTIME ${PCK}_OIIO_UTIL_RUNTIME)
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${PCK}
-  REQUIRED_VARS ${PCK}_LIBRARY ${PCK}_UTIL_LIBRARY ${PCK}_INCLUDE_DIR
+  REQUIRED_VARS ${REQ_VARS}
 )
 
 if(${PCK}_FOUND)
   set(${PCK}_LIBRARIES   ${${PCK}_LIBRARY} ${${PCK}_UTIL_LIBRARY})
   set(${PCK}_INCLUDE_DIRS ${${PCK}_INCLUDE_DIR})
+  if(WIN32)
+    set(${PCK}_RUNTIME   ${${PCK}_OIIO_RUNTIME} ${${PCK}_OIIO_UTIL_RUNTIME})
+  endif()
 endif()
